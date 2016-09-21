92e4d2b0d38899ddddfa77eb49708c7da2c04712
Zach Allen <zallen.work@gmail.com>
2016-09-21 11:34:13 -0500

    Add files via upload

---
+++ phaserTest/_js/game.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(960, 640, Phaser.AUTO, 'game-container', { preload: preload, create: create, update: update });
+
+function preload() {
+	
+}
+
+function create() {
+	
+}
+
+function update() {
+	
+}
